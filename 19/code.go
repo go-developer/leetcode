aeedf756c0e91c45fa327238a179e9ae041a250e
张德满 <go_developer@163.com>
2019-10-10 18:06:53 +0800

    update

--- 19/code.go
+++ 19/code.go
@@ -10,11 +10,10 @@ type ListNode struct {
 }
 
 /**
- * 删除链表倒数第n个节点
+ * 删除链表倒数第n个节点(占用空间大)
  * @author go_developer@163.com
  */
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	inputHead := head
 	dataList := make([]*ListNode, 0)
 	for nil != head.Next {
 		dataList = append(dataList, head)
@@ -25,7 +24,7 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 		return nil
 	}
 	if n == 0 {
-		return inputHead
+		return dataList[0]
 	}
 
 	if n == len(dataList) {
@@ -37,8 +36,8 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 
 	if n == 1 {
 		dataList[len(dataList) - 2].Next = nil
-		return inputHead
+		return dataList[0]
 	}
 	dataList[len(dataList) - n - 1].Next = dataList[len(dataList) - n + 1]
-	return inputHead
+	return dataList[0]
 }
