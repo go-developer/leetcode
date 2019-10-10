59c559611b466f1fdb09ff9ba3ad820ebf17f4ba
张德满 <go_developer@163.com>
2019-10-10 12:43:46 +0800

    完成11-盛水最多的容器

---
+++ 11/code.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+/**
+ * 盛水最多的容器
+ * @author go_developer@163.com
+ */
+func main()  {
+	var testCase = []int{2,3,4,5,18,17,6}
+	fmt.Println(maxArea(testCase))
+}
+
+/**
+ * 双指针法计算最大容量
+ * @author go_developer@163.com
+ */
+func maxArea(height []int) int {
+	left := 0	//左起点
+	right := len(height) - 1 //右起点
+	maxVal := 0 //最大容量
+	for left != right {
+		fmt.Println(left, " , ", right)
+		isLeftSmall := true //是否左边较小
+		currentHeight := height[left]
+		if currentHeight > height[right] {
+			isLeftSmall = false
+			currentHeight = height[right]
+		}
+		tmpVal := (right - left) * currentHeight
+		if tmpVal > maxVal {
+			maxVal = tmpVal
+		}
+
+		if isLeftSmall {
+			left = left + 1
+		} else {
+			right = right - 1
+		}
+	}
+	return maxVal
+}
